70e69e3458fcc0226a108b850556623d9d0e1204
Brian H <humphreybrian58@gmail.com>
2020-08-24 11:41:39 -0400

    first commit

---
+++ MathMethodExercise/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.ComponentModel;
+
+namespace MathMethodExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lets have some fun with addition!");
+
+            Console.WriteLine("Give me a number");
+            var firstNumber = Int32.Parse(Console.ReadLine());
+    
+
+            Console.WriteLine("Give me another");
+            var secondNumber = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your answer is " + add(firstNumber, secondNumber));
+
+        }
+
+        public static int add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static int add(int a, int b, int c)
+        {
+            return a + b + c;
+        }
+
+        public static int add(int a, int b, int c, int d)
+        {
+            return a + b + c + d;
+        }
+
+        public static int add(int a, int b, int c, int d, int e)
+        {
+            return a + b + c + d + e;
+        }
+    }
+}
